d56cd832a835400c694eaa9a4828a053e665b281
Russel <jruss1218a@gmail.com>
2025-03-09 00:32:54 +0800

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import '../styles/Footer.css';
 
 function Footer() {
- 
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <span className="beta-tag">Beta Testing Phase</span>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
